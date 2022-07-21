0defd3c4c68259a7728d849336b3e105ea8ee408
kat102 <katya.chernova28.04@gmail.com>
2022-07-21 10:35:23 +0300

    я добавил новое задание

--- src/Task4.java
+++ src/Task4.java
@@ -6,7 +6,10 @@ public class Task4 {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("write the number of the rows");
         int a = scanner1.nextInt();
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < a; i++) { // i текущая строчка, а всего строчек
+            for (int e = i; e < a ; e++) {
+                System.out.print(" ");
+            }
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
